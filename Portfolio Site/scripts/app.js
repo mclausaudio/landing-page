3e929d8ced1e8cdf8a2ed5ce1784252ef1adccd7
Michael_Claus <mclausaudio@gmail.com>
2019-04-22 13:38:58 -0700

    updates from end of 2018

--- Portfolio Site/scripts/app.js
+++ Portfolio Site/scripts/app.js
@@ -1,31 +1,64 @@
 // var SlideInOnScroll = require('./modules/SlideInOnScroll');
-require('../node_modules/waypoints/lib/noframework.waypoints.js');
-var waypoint = require('./modules/Waypoint');
+require("../node_modules/waypoints/lib/noframework.waypoints.js");
+var waypoint = require("./modules/Waypoint");
 // const $ = require("jquery");
 
-waypoint('about-text', ()=>{
-  document.getElementById('about-text').classList.add('slideInFromScroll-left__visible');
-}, '60%')
+waypoint(
+	"about-text",
+	() => {
+		document
+			.getElementById("about-text")
+			.classList.add("slideInFromScroll-left__visible");
+	},
+	"60%"
+);
 
-waypoint('portfolio-grid-1', ()=>{
-  document.getElementById('portfolio-grid-1').classList.add('slideInFromScroll-left__visible');
-}, '60%')
+waypoint(
+	"portfolio-grid-1",
+	() => {
+		document
+			.getElementById("portfolio-grid-1")
+			.classList.add("slideInFromScroll-left__visible");
+	},
+	"60%"
+);
 
-waypoint('portfolio-grid-2', ()=>{
-  document.getElementById('portfolio-grid-2').classList.add('slideInFromScroll-right__visible');
-}, '80%')
+waypoint(
+	"portfolio-grid-2",
+	() => {
+		document
+			.getElementById("portfolio-grid-2")
+			.classList.add("slideInFromScroll-right__visible");
+	},
+	"80%"
+);
 
-waypoint('music-frame-left', ()=>{
-  document.getElementById('music-frame-left').classList.add('slideInFromScroll-left__visible');
-}, '60%')
+waypoint(
+	"music-frame-left",
+	() => {
+		document
+			.getElementById("music-frame-left")
+			.classList.add("slideInFromScroll-left__visible");
+	},
+	"60%"
+);
 
-waypoint('music-frame-center', ()=>{
-  document.getElementById('music-frame-center').classList.add('slideInFromScroll-up__visible');
-}, '60%')
+waypoint(
+	"music-frame-center",
+	() => {
+		document
+			.getElementById("music-frame-center")
+			.classList.add("slideInFromScroll-up__visible");
+	},
+	"60%"
+);
 
-waypoint('music-frame-right', ()=>{
-  document.getElementById('music-frame-right').classList.add('slideInFromScroll-right__visible');
-}, '60%')
-
-
-console.log("hi from app js, after var waypoint");   
+waypoint(
+	"music-frame-right",
+	() => {
+		document
+			.getElementById("music-frame-right")
+			.classList.add("slideInFromScroll-right__visible");
+	},
+	"60%"
+);
